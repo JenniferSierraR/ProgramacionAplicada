825a4240310c6acd511994b693233ef30df796c7
JenniferSierraR <142939391+JenniferSierraR@users.noreply.github.com>
2023-09-17 17:45:42 -0500

    Create actualizacionInt.py

---
+++ PrimerCorte/actualizacionInt.py
@@ -0,0 +1,19 @@
+pan_precio = 3.12
+numero_de_panes = 4
+ 
+# valor total por los cuatro panes
+print(pan_precio * numero_de_panes)
+# percio del pan
+print(pan_precio)
+# numero de panes
+print(numero_de_panes)
+ 
+# actualizacion de precios
+pan_precio = 2.00
+ 
+# valor total por los cuatro panes a nuevo precio
+print(pan_precio * numero_de_panes)
+# imprime el nuevo valor del pan
+print(pan_precio)
+# imprime el numero de panes
+print(numero_de_panes)
